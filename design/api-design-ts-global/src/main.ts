0de026b6d7b9cf0f2ddfd1b99374ecbaa3c6a552
chenzhilin <chenzhilin@datapp.com.cn>
2024-07-01 16:21:53 +0800

    fix: 区分design和test目录

---
+++ design/api-design-ts-global/src/main.ts
@@ -0,0 +1,25 @@
+import { useRequest } from 'alova';
+
+// test
+const method = Apis.user.userLogin(
+  { username: '', password: '' },
+  {
+    name: '123',
+    params: {
+      a: 1,
+      b: 2
+    },
+    cache: 'force-cache',
+    credentials: 'same-origin'
+    // transformData(data, headers) {
+    //   return !!data.success;
+    // }
+  }
+);
+
+const { data, loading, send } = useRequest(method, {
+  initialData: {},
+  immediate: false
+});
+
+Apis.pet.getPetById({ petId: 1 });
