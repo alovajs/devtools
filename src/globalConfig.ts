a967057f2576700bc63004c4cafd73754576dfc4
chenzhilin <chenzhilin@datapp.com.cn>
2024-09-04 18:22:07 +0800

    chore: templates移动到packages里

--- src/globalConfig.ts
+++ src/globalConfig.ts
@@ -1,3 +1,4 @@
+/* eslint-disable import/prefer-default-export */
 import Error from '@/components/error';
 import { getTypescript, log } from '@/utils/work';
 import { setGlobalConfig } from '@alova/wormhole';
@@ -5,13 +6,8 @@ import path from 'node:path';
 import { pathToFileURL } from 'node:url';
 // work.js线程路径
 export const WORK_PATH = pathToFileURL(path.join(__dirname, '/work.js'));
-// 渲染模板路径
-// export const TEMPLATE_PATH = path.join(__dirname, '../templates');
-// alova临时文件路径
-export const ALOVA_TEMP_PATH = path.join('node_modules/.alova');
 // 全局配置
 setGlobalConfig({
-  alovaTempPath: ALOVA_TEMP_PATH,
   log,
   getTypescript,
   Error
