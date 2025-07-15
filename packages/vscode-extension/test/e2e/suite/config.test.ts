d737fc0343c2ce0abda4c25c7a06bc4ce9774127
chenzhilin <chenzhilin@datapp.com.cn>
2025-07-15 11:53:21 +0800

    chore(vscode-extension): remove unused mockCreateConfig setup in config tests

--- packages/vscode-extension/test/e2e/suite/config.test.ts
+++ packages/vscode-extension/test/e2e/suite/config.test.ts
@@ -1,18 +1,10 @@
 import * as assert from 'node:assert'
-import * as wormhole from '@alova/wormhole'
 import sinon from 'sinon'
 import * as vscode from 'vscode'
 
 describe('alova.create.config command', function () {
   this.timeout(1000000) // Set a timeout to avoid VSCode startup timeout
   vscode.window.showInformationMessage('Start all tests.')
-
-  let mockCreateConfig: sinon.SinonStub
-  this.beforeEach(() => {
-    // Create spies and stubs
-    mockCreateConfig = sinon.stub(wormhole, 'createConfig')
-  })
-
   this.afterEach(() => {
     // Restore all stubs
     sinon.restore()
@@ -24,7 +16,6 @@ describe('alova.create.config command', function () {
     // });
     // Mock the paths returned by getWorkspacePaths
     // const mockPaths = ['/path/to/project'];
-    mockCreateConfig.returns(Promise.resolve(undefined))
 
     // execute command
     // await vscode.commands.executeCommand('alova.create.config')
