5ace2a4bafee8b7c2f01aa7699ab40fa38687e74
chenzhilin <chenzhilin@datapp.com.cn>
2025-08-13 13:34:07 +0800

    refactor(wormhole): createApis支持api对象proxy缓存，减少内存压力

--- examples/js-commonjs-v2/alova.config.js
+++ examples/js-commonjs-v2/alova.config.js
@@ -1,72 +1,68 @@
-// alova.config.js
+// For more config detailed visit:
+// https://alova.js.org/tutorial/getting-started/extension-integration
+/**
+ * @type { import('@alova/wormhole').Config }
+ */
 module.exports = {
-  // api生成设置，为数组，每项代表一个自动生成的规则，包含生成的输入输出目录、规范文件地址等等
-  // 目前只支持openapi规范，包括openapi的2.0和3.0格式，但目前只做3.0规范
   generator: [
     {
-      // openapi的json文件url地址
-      // input: 'https://petstore.swagger.io',
-      // input: 'https://generator3.swagger.io',
-      input: './openapi.yaml',
-      // input: 'http://localhost:3000/openapi.json',
-      // input: 'openapi/api.json' // 以当前项目为相对目录的本地地址
-      // input: 'http://192.168.5.123:8080' // 没有指向openapi文件时，必须配合platform参数使用
-      // 支持openapi的平台，目前先支持swagger、knife4j、yapi，默认为空
-      // 当指定了此参数后，input字段只需要指定文档的地址而不需要指定到openapi文件，减小使用门槛
-      // 不同平台，它的openapi文件地址不一样，根据平台标识去对应地址下读取文件即可。
+      /**
+       * file input. support:
+       * 1. openapi json file url
+       * 2. local file
+       */
+      input: 'https://generator3.swagger.io',
+
+      /**
+       * input file platform. Currently only swagger is supported.
+       * When this parameter is specified, the input field only needs to specify the document address without specifying the openapi file
+       */
       platform: 'swagger',
 
-      // 接口文件和类型文件的输出路径，多个generator不能重复的地址，否则生成的代码会相互覆盖，无意义
+      /**
+       * output path of interface file and type file.
+       * Multiple generators cannot have the same address, otherwise the generated code will overwrite each other.
+       */
       output: 'src/api',
 
-      // （具体看下面）指定生成的响应数据的mediaType，指定后以此数据类型来生成200状态码的响应ts格式，默认application/json
-      responseMediaType: 'application/json',
+      /**
+       * the mediaType of the generated response data. default is `application/json`
+       */
+      // responseMediaType: 'application/json',
 
-      // （具体看下面）指定生成的请求体数据的mediaType，指定后以此数据类型来生成请求体的ts格式，默认application/json
-      bodyMediaType: 'application/json',
+      /**
+       * the bodyMediaType of the generated request body data. default is `application/json`
+       */
+      // bodyMediaType: 'application/json',
 
-      // 生成代码的类型，可选值为auto/ts/typescript/module/commonjs，默认为auto，会通过一定规则判断当前项目的类型
-      // ts/typescript：意思相同，表示生成ts类型文件
-      // module：生成esModule规范文件
-      // commonjs：表示生成commonjs规范文件
-      // type: 'module',
+      /**
+       * the generated api version. options are `2` or `3`, default is `auto`.
+       */
+      // version: 'auto',
 
-      // （具体看下面）过滤或转换生成的api接口函数，返回一个新的apiDescriptor来生成api调用函数
-      // 未指定此函数时则不转换apiDescripor对象
-      // apiDescriptor的格式与openapi文件的接口对象格式相同
-      // 对类型生成也同样适用123
-      handleApi: (apiDescriptor) => {
-        // 返回空表示过滤掉此api
-        // if (!apiDescriptor.url.startsWith('/generate')) {
-        //   return;
-        // }
-        // apiDescriptor.parameters = apiDescriptor.parameters.filter(param => param.in === 'path');
-        // if (apiDescriptor?.requestData?.properties) {
-        //   delete apiDescriptor.requestData.properties.type;
-        // }
-        // apiDescriptor.responses.properties['test'] = {
-        //   type: 'string'
-        // };
-        // apiDescriptor.url = apiDescriptor.url.replace('/generate', 'xxx1');
-        // if (apiDescriptor.parameters.length > 0) {
-        //   apiDescriptor.parameters = apiDescriptor.parameters.slice(-1);
-        // }
-        // if (apiDescriptor.responses.properties) {
-        //   apiDescriptor.responses.properties['test'] = {
-        //     type: 'string'
-        //   };
-        // }
-        return apiDescriptor
-      },
+      /**
+       * type of generated code. The options are `auto/ts/typescript/module/commonjs`
+       */
+      // type: 'auto',
+
+      /**
+       * exported global api name, you can access the generated api globally through this name, default is `Apis`.
+       * it is required when multiple generators are configured, and it cannot be repeated
+       */
+      // global: 'Apis',
+
+      /**
+       * filter or convert the generated api information, return an apiDescriptor, if this function is not specified, the apiDescriptor object is not converted
+       */
+      // handleApi: apiDescriptor => {
+      //  return apiDescriptor;
+      // }
     },
   ],
 
-  // 是否自动更新接口，默认开启，每5分钟检查一次，false时关闭
-  autoUpdate: false,
-  // autoUpdate: {
-  //   // 编辑器开启时更新，默认false
-  //   launchEditor: true,
-  //   // 自动更新间隔，单位秒
-  //   interval: 10
-  // }
+  /**
+   * extension only
+   * whether to automatically update the interface, enabled by default, check every 5 minutes, closed when set to `false`
+   */
+  // autoUpdate: true
 }
