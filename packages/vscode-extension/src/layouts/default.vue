21fe9a472f6f3559299db3bbbd6976c937323a0d
Scott Hu <huzhen555@qq.com>
2026-09-24 16:18:30 +0800

    fix: optimize changeview's layout

--- packages/vscode-extension/src/layouts/default.vue
+++ packages/vscode-extension/src/layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <main class="max-w-5xl min-w-[320px] mx-auto relative h-screen text-gray-700 dark:text-gray-200">
+  <main class="min-w-[320px] mx-auto relative h-screen text-gray-700 dark:text-gray-200">
     <RouterView />
   </main>
 </template>
